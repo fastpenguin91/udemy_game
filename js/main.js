0fcdc7de480ed73ff27a6ed241b8b77243cf264c
John Curry <johnc@tapiaproductionsinc.com>
2020-04-02 15:48:34 -0700

    dude created

--- js/main.js
+++ js/main.js
@@ -30,6 +30,16 @@ var createScene = function () {
     var followCamera = createFollowCamera(scene, tank);
     scene.activeCamera = followCamera;
     createLights(scene);
+
+    BABYLON.SceneLoader.ImportMesh("him", "models/Dude/", "Dude.babylon", scene, onDudeImported);
+    function onDudeImported(newMeshes, particleSystems, skeletons) {
+        newMeshes[0].position = new BABYLON.Vector3(0,0,5); // the original dude
+        console.log(skeletons.length);
+        //scene.beginAnimation(skeletons[0], 0, 120, 1.0, true);
+        scene.beginAnimation(skeletons[0],0,120,true,1.0);
+    }
+
+
     return scene;
 };
 
