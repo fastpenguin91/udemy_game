460e7aec80d6fd2799fe291e4e192f65415a3de9
John Curry <johnc@tapiaproductionsinc.com>
2020-04-03 10:55:53 -0700

    bounding boxes 17 18 19

--- js/main.js
+++ js/main.js
@@ -9,18 +9,39 @@ var isEPressed = false;
 document.addEventListener("DOMContentLoaded", startGame);
 
 class Dude {
-    constructor(dudeMesh,speed)
+    constructor(dudeMesh,speed, id, scene, scaling)
     {
         this.dudeMesh = dudeMesh;
+        this.id = id;
+        this.scene = scene;
         dudeMesh.Dude = this;
-        if (speed)
+
+        if (speed) {
             this.speed = speed;
-        else 
+        } else { 
             this.speed = 1;
+        }
+        if (scaling) {
+            this.scaling = scaling;
+            this.dudeMesh.scaling = new BABYLON.Vector3(this.scaling, this.scaling, this.scaling);
+        } else {
+            this.scaling = 1;
+        }
+        if(Dude.boundingBoxParameters == undefined)
+        {
+            Dude.boundingBoxParameters = this.calculateBoundingBoxParameters();
+        }
+
+        this.bounder = this.createBoundingBox();
+        this.bounder.dudeMesh = this.dudeMesh;
     }
 
     move()
     {
+
+        if (!this.bounder) return;
+        this.dudeMesh.position = new BABYLON.Vector3(this.bounder.position.x,
+            this.bounder.position.y - this.scaling * Dude.boundingBoxParameters.lengthY/2.0, this.bounder.position.z);
         var tank = scene.getMeshByName("heroTank");
         var direction = tank.position.subtract(this.dudeMesh.position);
         var distance = direction.length(); //don't let dude get to tank?
@@ -29,11 +50,78 @@ class Dude {
         this.dudeMesh.rotation.y = alpha;
         if(distance > 30) {
             console.log("moving");
-            this.dudeMesh.moveWithCollisions(dir.multiplyByFloats(this.speed, this.speed, this.speed));
-        } else {
-            console.log("not moving");
+            this.bounder.moveWithCollisions(dir.multiplyByFloats(this.speed, this.speed, this.speed));
         }
 
+    }
+
+    createBoundingBox()
+    {
+        var lengthX = Dude.boundingBoxParameters.lengthX;
+        var lengthY = Dude.boundingBoxParameters.lengthY;
+        var lengthZ = Dude.boundingBoxParameters.lengthZ;
+
+        var bounder = new BABYLON.Mesh.CreateBox("bounder" + (this.id).toString(), 1, this.scene);
+
+        bounder.scaling.x = lengthX * this.scaling;
+        bounder.scaling.y = lengthY * this.scaling;
+        bounder.scaling.z = lengthZ * this.scaling * 2;
+
+        bounder.isVisible = false;
+
+        var bounderMaterial = new BABYLON.StandardMaterial("bounderMaterial", this.scene);
+        bounderMaterial.alpha = .5;
+        bounder.material = bounderMaterial;
+        bounder.checkCollisions = true;
+
+        bounder.position = new BABYLON.Vector3(this.dudeMesh.position.x, this.dudeMesh.position.y + this.scaling * lengthY/2, this.dudeMesh.position.z);
+
+
+        return bounder;
+
+    }
+
+    calculateBoundingBoxParameters()
+    {
+        var minX = 999999; var minY = 999999; var minZ = 999999;
+        var maxX = -99999; var maxY = -99999; var maxZ = -99999;
+
+        var children = this.dudeMesh.getChildren();
+
+        for (var i = 0; i < children.length; i++ )
+        {
+            var positions = new BABYLON.VertexData.ExtractFromGeometry(children[i]).positions;
+            if(!positions) continue;
+
+            var index = 0;
+            for(var j = index; j < positions.length; j +=3) {
+                if (positions[j] < minX)
+                    minX = positions[j];
+                if( positions[j] > maxX)
+                    maxX = positions[j];
+            }
+            index = 1;
+            for(var j = index; j < positions.length; j +=3) {
+                if (positions[j] < minY)
+                    minY = positions[j];
+                if( positions[j] > maxY)
+                    maxY = positions[j];
+            }
+            index = 2;
+            for(var j = index; j < positions.length; j +=3) {
+                if (positions[j] < minZ)
+                    minZ = positions[j];
+                if( positions[j] > maxZ)
+                    maxZ = positions[j];
+            }
+
+            var _lengthX = maxX - minX;
+            var _lengthY = maxY - minY;
+            var _lengthZ = maxZ - minZ;
+
+        }
+
+        return {lengthX : _lengthX , lengthY: _lengthY , lengthZ : _lengthZ};
     }
 }
 
@@ -166,16 +254,16 @@ function createHeroDude(scene)
         newMeshes[0].position = new BABYLON.Vector3(0,0,5); // the original dude
         newMeshes[0].name = "heroDude";
         var heroDude = newMeshes[0];
-        heroDude.scaling = new BABYLON.Vector3(.2,.2,.2);
-        heroDude.speed = 2;
+        
         scene.beginAnimation(skeletons[0],0,120,true,1.0);
-        var hero = new Dude(heroDude, 2);
+        var hero = new Dude(heroDude, 2, -1, scene,.2);
 
         scene.dudes = [];
         for ( var q = 0; q< 10; q++) {
             scene.dudes[q] = DoClone(heroDude, skeletons, q);
             scene.beginAnimation(scene.dudes[q].skeleton, 0, 120, true, 1.0);
-            var temp = new Dude(scene.dudes[q], 2);
+            var temp = new Dude(scene.dudes[q], 2, q, scene, .2);
+
         }
     }
 
